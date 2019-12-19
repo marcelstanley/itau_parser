9c929a2c2296d0c14ec810a758afd2c7a3b3fbc7
Marcel Moura <marcel.stanley@gmail.com>
2019-12-19 11:50:12 -0300

    Adjusted output format to Goggle sheets template

--- parse_itau.lua
+++ parse_itau.lua
@@ -9,36 +9,72 @@ if #arg ~= 2 then
   os.exit()
 end
 
-function convert_line(inline)
+header = {
+  ',Change or add categories by updating the Expenses and Income tables in the Summary sheet.,,,,,,,,',
+  ',Expenses,,,,,Income,,,',
+  ',,,,,,,,,',
+  ',Date,Amount,Description,Category,,Date,Amount,Description,Category',
+}
+income = {}
+expenses = {}
+
+function parse_line(inline)
   discard, orig_date, entry, orig_amount = string.match(inline, "((%d%d/%d%d/%d%d%d%d);([^;]+);([-,%w]+))")
 
   date = string.gsub(orig_date, '((%d%d)/(%d%d)/(%d%d%d%d))', '%3/%2/%4');
   amount = string.gsub(orig_amount,',','.')
 
-  return date..';'..entry..';'..amount  
+  if tonumber(amount) >= 0 then
+    table.insert(income, date..','..amount..','..entry) 
+  else
+    table.insert(expenses, date..','..math.abs(amount)..','..entry) 
+  end
 end
 
-function convert_file(infile, outfile)
-  print('Converting '..infile..' to '..outfile..'...')
-
+function parse_file(infile, outfile)
   input = io.open(infile)
   io.input(input)
 
-  output = io.open(outfile, 'w+')
-  io.output(output)
-
   line = io.read()
   while line ~= nil do
-    io.write(convert_line(line))
-    io.write('\n')
+    parse_line(line)
     line = io.read()
   end
 
   io.close(input)
-  io.close(output)
-
-  print('DONE')
-
 end
 
-convert_file(arg[1], arg[2])
+function print_line(line)
+  if line ~= nil then io.write(line..'\n') end
+end
+
+function print_file(outfile)
+  output = io.open(outfile, 'w+')
+  io.output(output)
+  
+  print_line(header[1])
+  print_line(header[2])
+  print_line(header[3])
+  print_line(header[4])
+
+  if #expenses >= #income then
+    i = 1
+    while i <= #expenses do
+      line = ','..expenses[i]..',Other'
+      if i <= #income then
+        line = line..',,'..income[i]..',Other'
+      else
+        line = line..',,,,,'
+      end
+      print_line(line)
+      i = i + 1
+    end
+  else
+
+  end  
+
+  io.close(output)
+end
+
+parse_file(arg[1])
+print_file(arg[2])
